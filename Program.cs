3d4466004bc45758de3e97451a7a430c4440a1f0
Eduarda Ferreira <edududaferreira10@gmail.com>
2022-10-15 14:10:54 -0300

    Resolução

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿int primeiroNumero, segundoNumero, soma;
+
+Console.WriteLine("Cálculo da soma entre dois números.\n");
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write("Digite o primeiro número: ");
+primeiroNumero = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write("Digite o segundo número: ");
+segundoNumero = Convert.ToInt32(Console.ReadLine());
+Console.ResetColor();
+
+soma = primeiroNumero + segundoNumero;
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($"\nSoma: {soma}");
+Console.ResetColor();
